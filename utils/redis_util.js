a7e34362814f957bfe3df74674712c4d21cbe41f
JansenGao <298066717@qq.com>
2018-05-15 14:51:34 +0800

    用redis缓存access_token

--- utils/redis_util.js
+++ utils/redis_util.js
@@ -2,68 +2,56 @@ const redis = require('redis');
 var logger = require('./logger').logger;
 const https = require('https');
 const config = require('../config/config');
-const Bluebird = require('bluebird');
 
 var env = config.environment;
 const redis_host = config[env].redis.host;
 const redis_port = config[env].redis.port;
 const client = redis.createClient(redis_port, redis_host);
 
-clientPromise = new Bluebird(((resolve, reject) => {
-    client.get('access_token_wxf22daa784d9b9b02', (err, reply) => {
-        resolve(reply);
-    });
-}));
-
-function cache_access_token(app_id, app_secret){
-    key = "access_token_" + app_id;
-
-    return client.get(key, function(err, reply){
-        if(err){
-            logger.error('Redis connection failed.');
-            return null;
-        }else{
-            if(reply){
-                console.log('Has cache: %s', reply);
-                return reply;
+/*
+    返回一个Promise对象，用then((token) => {})获取access_token
+*/
+exports.cache_access_token = function(app_id, app_secret){
+    var that = this;
+    return new Promise(function(resolve, reject){
+        key = "access_token_" + app_id;
+        access_token = '';
+        client.get(key, (err, access_token) => {
+            if(err){
+                return reject(err);
+            }
+            if(access_token){
+                return resolve(access_token);
             }else{
-                console.log('Has no cache');
+                console.log('Token not found in Redis, use https to get.')
                 var URL = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential';
                 URL += '&appid=' + app_id;
                 URL += '&secret=' + app_secret;
-                console.log(URL);
-
-                value = '';
 
                 https.get(URL, res => {
                     var buffers = [];
-                    console.log(res.statusCode);
                     res.on('data', chunk => {
-                        // console.log(chunk);
                         buffers.push(chunk);
                     });
 
                     res.on('end', chunk => {
                         var whole = Buffer.concat(buffers);
                         var dataStr = whole.toString('utf8');
-                        console.log(dataStr);
                         data = JSON.parse(dataStr);
-                        value = data.access_token;
-                        client.set(key, data.access_token, 'EX',  data.expires_in, (err, reply) => {
+                        access_token = data.access_token;
+                        client.set(key, access_token, 'EX',  data.expires_in, (err, reply) => {
                             if(err){
-                                console.log(err);
+                                return reject(err);
+                            }else{
+                                console.log('Token saved in Redis.')
+                                return resolve(access_token);
                             }
                         });
                     });
-                }).on('err', err => {
-                    console.log(err);
-                }).on('end', {
-
+                }).on('err', (err) => {
+                    return reject(err);
                 });
-                return value;
             }
-        }
+        });
     });
 }
-
-exports.cache_access_token = cache_access_token;
